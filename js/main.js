d1adae8df82b46aed32371ab4606d9a1be46f520
anthonyaubrey <anthonyaubrey245@gmail.com>
2016-03-08 17:36:15 -0500

    Added more vars

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
-var $newh1 = $('<h1>');                     // Make a new HTML tag, notice the `<>`
-$newh1.html('Below ground veggies');
-$('body').prepend($newh1);
+var $newH1 = $('<H1>');
+$newH1.html('Below ground veggies');
+$('body').prepend($newH1);
+
+var $belowVeggies = $('<ul>');
+$('body').append($belowVeggies);
+
+var $aboveH1 = $('<h1>');
+$aboveH1.html('Above ground veggies');
+$('body').append($aboveH1);
+
+var $aboveVeggies = $('<ul>');
+$('body').append($aboveVeggies);
+
+$('ul li').each(function () {
+  if ($(this).hasClass('below')){
+  $belowVeggies.append($(this));
+  } else {
+  $aboveVeggies.append($(this));
+}
+
+});
